6cb10c0d6aa23b5f13312a257fb8da1e83e48ae9
Marcelo Magalhaesm <celoxd01@gmail.com>
2025-04-16 20:38:58 -0300

    A0009_type_enumA008

---
+++ src/A0009_type_enum/A0009_type_enum.ts
@@ -0,0 +1,21 @@
+enum Colors {
+  RED = 10, //0
+  BLUE = 100, //1
+  YELLOW = 200, // 2
+}
+enum Colors {
+  PURPLE = 'PURLE',
+  GREEN = 201,
+  PINK = 500,
+}
+
+// console.log(Colors.RED);
+// console.log(Colors[10]); // RED
+// console.log(Colors.PURPLE);
+
+console.log(Colors);
+
+// function selectColor(color: Colors): void {
+//   console.log(Colors[color]);
+// }
+// selectColor(Colors.PURPLE);
