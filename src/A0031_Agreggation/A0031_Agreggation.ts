c0d67c7f2d316c911654197c6aa7ef2cfb4f88a4
Marcelo Magalhaesm <celoxd01@gmail.com>
2025-04-18 18:03:38 -0300

    Agreggation

---
+++ src/A0031_Agreggation/A0031_Agreggation.ts
@@ -0,0 +1,31 @@
+export class ShopCart {
+  private readonly products: Product[] = [];
+
+  insertProducts(...products: []): void {
+    for (const product of products) {
+      this.products.push(product);
+    }
+  }
+  quantityProducts(): number {
+    return this.products.length;
+  }
+  totalValue(): number {
+    return this.products.reduce((sum, product) => sum + product.price, 0);
+  }
+}
+
+export class Product {
+  constructor(
+    public name: string,
+    public price: number,
+  ) {}
+}
+
+const product1 = new Product('Short', 49.9);
+const product2 = new Product('Pend', 9.9);
+const product3 = new Product('Pendrive', 4.9);
+
+const shopCart = new ShopCart();
+shopCart.insertProducts(product1, product2, product3);
+console.log(shopCart.totalValue());
+console.log(shopCart.quantityProducts());
