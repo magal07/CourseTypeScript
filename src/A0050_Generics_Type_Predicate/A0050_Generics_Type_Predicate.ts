f77088ad0c19418b34a7147677912544e5625883
Marcelo Magalhaesm <celoxd01@gmail.com>
2025-04-22 15:55:03 -0300

    Generics - Predicate

---
+++ src/A0050_Generics_Type_Predicate/A0050_Generics_Type_Predicate.ts
@@ -0,0 +1,17 @@
+export function isNumber(value: unknown): value is number {
+  return typeof value === 'number';
+}
+
+export function sum<T>(...args: T[]): number | null {
+  const result = args.reduce((sum, value) => {
+    if (isNumber(sum) && isNumber(value)) {
+      return sum + value;
+    }
+    return sum;
+  }, 0);
+  return result;
+}
+console.log(sum(1, 2, 3));
+console.log(sum(1, 2, 3, 'a'));
+console.log(sum('a', 'b', 'c'));
+console.log(sum(...[1, 2, 3, 'a', 'b', 'c', 1]));
